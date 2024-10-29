a03013303badbfe9471f327b691f1d549a951096
Sardius <0xsardius@gmail.com>
2024-10-29 16:59:39 -0400

    shadcn install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main>
+    <main className="">
       <h1>LFGGGGGG</h1>
+
+      <Button>Click me</Button>
     </main>
   );
 }
